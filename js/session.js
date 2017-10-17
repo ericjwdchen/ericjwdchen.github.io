8e7cd10ac998c03d5ea401f8fbb5ec9fa55ce250
ericjwdchen <eric.jwd.chen@gmail.com>
2017-10-17 02:03:07 -0400

    a

--- js/session.js
+++ js/session.js
@@ -2,6 +2,13 @@
  * Get and send oauth tokens from query string.
  */
 
- chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
-   console.log(response.farewell);
- });
+// The ID of the extension we want to talk to.
+var editorExtensionId = "abcdefghijklmnoabcdefhijklmnoabc";
+
+
+chrome.management.getAll(function(items) {
+  for (var i = 0; i < items.length; i++) {
+    var item = items[i];
+    console.log(item.id + " : (" + item.type + ") " + item.name);
+  }
+});
